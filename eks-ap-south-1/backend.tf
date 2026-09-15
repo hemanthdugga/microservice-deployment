9486f0296ce1e584f7bc8aad9d3297862f815a6a
hemanthdugga <221865701+hemanthdugga@users.noreply.github.com>
2026-09-15 22:22:40 +0530

    Update S3 bucket name in backend configuration

--- eks-ap-south-1/backend.tf
+++ eks-ap-south-1/backend.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "s3-bucket-2-govind-kemla1"
+    bucket = "s3-bucket-22-i-hemanth-reddy"
     key    = "k8/terraform.tfstate"
     region = "ap-south-1"
   }
