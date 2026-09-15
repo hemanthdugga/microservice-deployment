430df3fe5b84027541cc66f7d83eb5c9a7558749
hemanthdugga <221865701+hemanthdugga@users.noreply.github.com>
2026-09-15 19:08:22 +0530

    my second commit

--- ecr-terraform/backend.tf
+++ ecr-terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.25.0"
+      version = "~> 5.100"
     }
   }
 
@@ -14,4 +14,3 @@ terraform {
 
   required_version = ">= 1.6.3"
 }
-
